fb80726884e443ee785a3675136e07215d77985b
Sikriti Dakua <destrodev123@gmail.com>
2023-11-09 13:12:40 +0530

    Initial commit

---
+++ src/components/Hello.svelte
@@ -0,0 +1,2 @@
+<h1>Hwllo World!</h1>
+<h2>Welcome to your library project</h2>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Hello } from '@/components';
+</script>
+
+<Hello />
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
