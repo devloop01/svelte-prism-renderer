7983d1b1ff8b8122663f1fef1280eb7441cabea9
Sikriti Dakua <destrodev123@gmail.com>
2023-11-09 13:12:42 +0530

    feat: added tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,14 @@
 </script>
 
 <Hello />
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
